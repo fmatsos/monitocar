9ebd89b9302c88dcd1739815f8b03f90ec6d8066
fmatsos <franck@matsos.fr>
2020-05-02 17:56:03 +0200

    Adding app.js for Webpack Encore.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,11 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+import 'admin-lte/plugins/fontawesome-free/css/all.min.css';
+import 'admin-lte/dist/css/adminlte.min.css';
+import 'admin-lte/plugins/toastr/toastr.min.css'
 
-// any CSS you import will output into a single css file (app.css in this case)
+import '../css/menu.css';
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
+import 'admin-lte/plugins/jquery/jquery.min';
+import 'admin-lte/plugins/bootstrap/js/bootstrap.bundle.min';
+import 'admin-lte/dist/js/adminlte.min.js';
 
+import 'admin-lte/plugins/toastr/toastr.min'
